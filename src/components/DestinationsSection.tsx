a23181a1c9097752b776719a38abea8fd4ab0423
Garvit Jain <garvitj2003@gmail.com>
2026-01-13 02:27:27 +0000

    fixed type erros

--- src/components/DestinationsSection.tsx
+++ src/components/DestinationsSection.tsx
@@ -51,7 +51,7 @@ export default function DestinationsSection() {
     );
   };
 
-  const getPosition = (index) => {
+  const getPosition = (index: number) => {
     const total = destinations.length;
     let offset = (index - activeIndex + total) % total;
     if (offset > 2) offset -= total;
